ec6f3b915030a00aca40e4fb67c8f0047c436ccf
DanielPuentee <daniel.puente@alumni.mondragon.edu>
2022-10-19 20:16:17 +0200

    comments are added to the class

--- outliers_detection.py
+++ outliers_detection.py
@@ -5,20 +5,37 @@ from scipy import stats
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+"""
+
+Outliers detection  package.
+It is a package generated to perform easy outlier detection for both dataframes and arrays. 
+It incorporates the use of graphics for a better understanding of these.
+
+"""
 
 class Outliersdetection:
     
+    """ The class do not requiere any argument """
+
     def __outliers_iqr(self, df, col):
+
+        """ With this private function we can detect outliers using the IQR method. """
+
         q1 = df[col].quantile(0.25)
         q3 = df[col].quantile(0.75)
         iqr = q3 - q1
         return list(df[(df[col] < q1 - 1.5 * iqr) | (df[col] > q3 + 1.5 * iqr)].index)
 
     def __outliers_zscore(self, df, col):
+
+        """ With this private function we can detect outliers using the zscore method. """
+
         z = np.abs(stats.zscore(df[col]))
         return list(df[(z > 3) | (z < -3)].index) 
 
     def __stripplot(self, df, col, palette):
+
+        """ A private function to make a plot of one numeric feature where outliers are indicated """
         
         title = col[0].capitalize()
         sns.stripplot(data=df, y=col[0], hue="outliers", palette=palette, size = df['size'])
@@ -27,14 +44,26 @@ class Outliersdetection:
         plt.show()
 
     def __dataframe_plot(self, df, col, outliers, size):
-            colname, outliers_tag, size = list(df[col[0]]), ['normal' if _ not in outliers[col[0]] else 'outlier' for _ in range(len(df))], [size[0] if _ not in outliers[col[0]] else size[1] for _ in range(len(df))]
-            new_df = pd.DataFrame([colname, outliers_tag, size]).T.rename(columns={0: col[0], 1: 'outliers', 2: 'size'})
-            new_df['size'] = new_df['size'].astype(int)
-            return new_df
+
+        """ A private function to create the dataframe necesary to use the __stripplot function """
+
+        colname, outliers_tag, size = list(df[col[0]]), ['normal' if _ not in outliers[col[0]] else 'outlier' for _ in range(len(df))], [size[0] if _ not in outliers[col[0]] else size[1] for _ in range(len(df))]
+        new_df = pd.DataFrame([colname, outliers_tag, size]).T.rename(columns={0: col[0], 1: 'outliers', 2: 'size'})
+        new_df['size'] = new_df['size'].astype(int)
+        return new_df
 
     ########################################################################################################################################################################################################################
 
-    def outliers(self, df, col, method):
+    def outliers(self, df, col = 'all', method = 'all'):
+
+        """ 
+        
+        This is one of the main function that requieres the 3 main arguments: df, col and method.
+        df: Could be a dataframe, list or an array.
+        col: The column to detect outliers. It can be a list or a string in case it is only one column. "all" is also a valid option to select every numeric column of the dataset (default = 'all').
+        method: The method to detect outliers. It can be "iqr", "zscore" or "all". "all" is the default option (default = 'all').
+        
+        """
 
         if type(df) != pd.core.frame.DataFrame:
             try: 
@@ -66,7 +95,19 @@ class Outliersdetection:
 
     ########################################################################################################################################################################################################################
 
-    def plot_outliers(self, df = None, col = None, method = 'all', size = [5, 7], palette = ((133/255, 202/255, 194/255), (38/255, 70/255, 83/255)) ):
+    def plot_outliers(self, df, col, method = 'all', size = [5, 7], palette = ((133/255, 202/255, 194/255), (38/255, 70/255, 83/255)) ):
+
+        """
+
+        This function is used to plot the outliers detected. It requieres the same arguments as the outliers function.
+        However, it includes 2 extra arguments to custome the striplot
+        df: Could be a dataframe, list or an array.
+        col: The column to detect outliers. It just can be 1 numeric column in string or list format.
+        method: The method to detect outliers. It can be "iqr", "zscore" or "all". "all" is the default option (default = 'all').
+        size: A list with the size of the points. The first value is for the normal points and the second for the outliers (default = [5, 7]).
+        palette: A tuple with the colors to use in the plot. The first value is for the normal points and the second for the outliers (default = ((133/255, 202/255, 194/255), (38/255, 70/255, 83/255))).
+
+        """
 
         if type(df) != pd.core.frame.DataFrame:
             try: 
@@ -107,9 +148,9 @@ class Outliersdetection:
         else: raise ValueError('The column may not exist or may not be numeric')
         
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
     
-    df = pd.read_csv('spotify_cl.csv')
-    od = Outliersdetection()
-    print(od.outliers(np.array(df), 'all', method = 'iqr'))
-    od.plot_outliers(np.array(df), ['5'], method = 'all')
+#     df = pd.read_csv('spotify_cl.csv')
+#     od = Outliersdetection()
+#     print(od.outliers(np.array(df), 'all', method = 'iqr'))
+#     od.plot_outliers(np.array(df), ['5'], method = 'all')
